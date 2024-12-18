3161d6dc163b5aa1896cbcd1eba3f272d0ee5cb5
Sofia <katzmansof@gmail.com>
2024-12-17 23:34:34 -0500

    adjust for index of 0

--- src/SwitchReel.jsx
+++ src/SwitchReel.jsx
@@ -1,32 +1,31 @@
 import { useState } from 'react';
 
-function SwitchReel({ categoryName }) {
-  const [item, setItem] = useState(1);
+function SwitchReel({ categoryName, data }) {
+  const [item, setItem] = useState(0);
 
+  //Navigation Functions
   function goBack() {
-    if (item === 1) {
-      setItem(4); 
+    if (item === 0) {
+      setItem(3); 
     } else {
       setItem(item - 1); 
     }
   }
 
   function goForth() {
-    if (item === 4) {
-      setItem(1); 
+    if (item === 3) {
+      setItem(0); 
     } else {
       setItem(item + 1); 
     }
   }
 
   return (
-    <div style={{ textAlign: 'center', margin: '20px' }}>
-      {/* Placeholder image that updates dynamically */}
-      {/* <img
-        src={`https://via.placeholder.com/150?text=Item+${item}`}
-        alt={`Item ${item}`}
-        style={{ display: 'block', margin: '0 auto 10px' }}
-      /> */}
+    <div>
+      <img
+        src={data[item].src}
+        alt={`${categoryName} ${item}`}
+      />
       <div>
         <button onClick={goBack}>Back</button>
         <p>
